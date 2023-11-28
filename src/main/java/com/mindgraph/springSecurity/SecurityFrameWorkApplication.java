06eb72bfb92c76a4ce7ca2631d890becbe919c61
simson-m <simson.m@mind-graph.com>
2023-11-28 16:25:28 +0530

    init

---
+++ src/main/java/com/mindgraph/springSecurity/SecurityFrameWorkApplication.java
@@ -0,0 +1,13 @@
+package com.mindgraph.springSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityFrameWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityFrameWorkApplication.class, args);
+	}
+
+}
